32b4e6d37cda705719811c9988e42d6116378bec
LauroAranha <lauroaranha.0@gmail.com>
2022-04-13 23:48:08 -0300

    Update

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,26 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './main/Home'
-import About from './main/About'
-import Ong from './main/Ong'
-import ErrorPage from './Error'
-import Nav from '../components/props/Nav'
-import Footer from '../components/props/Footer'
-import './Default.css'
+
+import AppRoutes from './AppRoutes'
+
 function App() {
-  return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/ong" element={<Ong />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      <Footer />
-      
-    </Router>
-    
-  )
+  return <AppRoutes />
 }
 
 export default App
