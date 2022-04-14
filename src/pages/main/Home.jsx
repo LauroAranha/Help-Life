8e82930e5dadbd6404809236a9869bccce9c666b
LauroAranha <lauroaranha.0@gmail.com>
2022-04-13 23:47:15 -0300

    Update

--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import Nav from "../../components/props/Nav";
+import Footer from "../../components/props/Footer";
 import ContentMain from "../../components/main/ContentMain";
 
 export default class Main extends Component {
   render() {
     return (
       <div>
+        <Nav />
         <ContentMain />
+        <Footer />
       </div>
     );
   }
--- src/pages/main/Ong.jsx
+++ src/pages/main/Ong.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
+import Nav from "../../components/props/Nav";
+import Footer from "../../components/props/Footer";
 import ContentOng from "../../components/ong/ContentOng";
-
 
 export default class Main extends Component {
   render() {
     return (
       <div>
+        <Nav />
         <ContentOng />
+        <Footer />
       </div>
     );
   }
